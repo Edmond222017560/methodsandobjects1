1e5fb6957505dd0f76c629b42e515524778ef27f
Edmond222017560 <149670234+Edmond222017560@users.noreply.github.com>
2023-11-08 13:12:58 -0800

    Add files via upload

---
+++ mypets.java
@@ -0,0 +1,48 @@
+// Interface for Pets
+interface Pets {
+    void speak();
+}
+
+// Class for Dog implementing the Pets interface
+class Dog implements Pets {   
+    public void speak() {
+        System.out.println("Dog says Woof!");
+    }
+}
+
+// Class for Cat implementing the Pets interface
+class Cat implements Pets { 
+    public void speak() {
+        System.out.println("Cat says Meow!");
+    }
+}
+
+// Class for Bird implementing the Pets interface
+class Bird implements Pets {
+    public void speak() {
+        System.out.println("Bird chirps!");
+    }
+}
+//class for Mouse implementing the Pets interface
+class Mouse implements Pets {
+    public void speak (){
+    System.out.println("Mouse squeaks ");   
+    }
+}
+
+// Class to demonstrate calling methods
+public class mypets {
+    public static void main(String[] args) {
+        Pets dog = new Dog();
+        Pets cat = new Cat();
+        Pets bird = new Bird();
+        Pets mouse = new Mouse();
+
+        dog.speak();
+        cat.speak();
+        bird.speak();
+        mouse.speak();
+    }
+}
+
+
---
+++ myshapes.java
@@ -0,0 +1,45 @@
+// Interface for Shape
+interface Shape {
+    void draw();
+}
+
+// Class for Circle implementing the Shape interface
+class Circle implements Shape {   
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
+
+// Class for Rectangle implementing the Shape interface
+class Rectangle implements Shape {  
+    public void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
+
+// Class for Triangle implementing the Shape interface
+class Triangle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a Triangle");
+    }
+}
+// class for Square implementing the shape interface
+class Square implements Shape {
+public void draw(){
+System.out. println("Drawing a Square");
+}
+}
+// Class to demonstrate calling methods
+public class myshapes {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape rectangle = new Rectangle();
+        Shape triangle = new Triangle();
+        Shape square = new Square();
+
+        circle.draw();
+        rectangle.draw();
+        triangle.draw();
+        square.draw();
+    }
+}
